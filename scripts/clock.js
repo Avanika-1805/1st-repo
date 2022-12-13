14b7fa2f6e50c8e95733607a767ce9274ccb1e64
Avanika-1805 <avanikaboppudi555@gmail.com>
2022-12-13 11:32:45 +0530

    work

---
+++ scripts/clock.js
@@ -0,0 +1,7 @@
+// function displayDate(){
+//     document.getElementById("showDate").innerHTML=Date();
+//     }
+//      console.log(displayDate());
+
+const display=document.getElementById("display");
+const time=document.getElementById("time");
---
+++ scripts/task.js
@@ -0,0 +1,55 @@
+const firstbox=document.getElementById("box1");
+const secondbox=document.getElementById("box2");
+const thirdbox=document.getElementById("box3");
+const fourthbox=document.getElementById("box4");
+const fifthbox=document.getElementById("box5");
+const sixthbox=document.getElementById("box6");
+const container=document.getElementsByClassName("container");
+
+
+document.getElementById("box1").innerHTML=`This is first box`;
+document.getElementById("box2").innerHTML=`This is second box`;
+document.getElementById("box3").innerHTML=`This is third box`;
+document.getElementById("box4").innerHTML=`This is fourth box`;
+document.getElementById("box5").innerHTML=`This is fifth box`;
+document.getElementById("box6").innerHTML=`This is sixth box`;
+
+firstbox.style.backgroundColor ="blue";
+firstbox.style.textAlign ="center";
+firstbox.style.width="350px";
+firstbox.style.height="350px";
+firstbox.style.float="left";
+
+secondbox.style.backgroundColor ="green";
+secondbox.style.textAlign ="center";
+secondbox.style.width="350px";
+secondbox.style.height="350px";
+secondbox.style.float="left";
+
+thirdbox.style.backgroundColor ="yellow";
+thirdbox.style.textAlign ="center";
+thirdbox.style.width="350px";
+thirdbox.style.height="350px";
+thirdbox.style.float="left";
+
+fourthbox.style.backgroundColor ="black";
+fourthbox.style.textAlign ="center";
+fourthbox.style.color="white";
+fourthbox.style.width="350px";
+fourthbox.style.height="350px";
+fourthbox.style.float="left";
+
+fifthbox.style.backgroundColor ="red";
+fifthbox.style.textAlign ="center";
+fifthbox.style.width="350px";
+fifthbox.style.height="350px";
+fifthbox.style.float="left";
+
+sixthbox.style.backgroundColor ="violet";
+sixthbox.style.textAlign ="center";
+sixthbox.style.width="350px";
+sixthbox.style.height="350px";
+sixthbox.style.float="left";
+
+
+
