915e042b8abb74adf9b08cbd9a8b61cb035ec075
Avanika-1805 <avanikaboppudi555@gmail.com>
2022-12-23 12:24:45 +0530

    async

--- scripts/asyc.js
+++ scripts/asyc.js
@@ -17,19 +17,15 @@ function getDevelopers() {
         })
     }, 2000);
 }
-// function createDeveloper(dev) {
-//     setTimeout(() => {
-//         developers.push(dev);
-//     }, 3000);
-// }
-// getDevelopers();
-// createDeveloper({
-//     name: "Ramesh",
-//     favFruit: "Orange",
-//     company: "Amazon"
-// });
-//Asynchronous using Callbacks
-/* function createDeveloper(dev, cb) {
+function createDeveloper(dev) {
+    setTimeout(() => {
+        developers.push(dev);
+    }, 3000);
+}
+getDevelopers();
+
+// Asynchronous using Callbacks
+ function createDeveloper(dev, cb) {
     setTimeout(() => {
         developers.push(dev);
         cb();
@@ -39,26 +35,16 @@ createDeveloper({
     name: "Ramesh",
     favFruit: "Orange",
     company: "Amazon"
-}, getDevelopers); */
+}, getDevelopers); 
 //Promises
-function createDeveloper(dev) {
-    return new Promise( (resolve, reject) => {
-        setTimeout(() => {
-            if(dev && Object.keys(dev).length === 0 && Object.getPrototypeOf(dev) === Object.prototype) {
-                reject("Empty Dev Receieved. Please Fix");
-            } else {
-                resolve(developers.push(dev));
-            }
-        }, 3000);
-    })
-}
-/*createDeveloper({
+
+createDeveloper({
     name: "Ramesh",
     favFruit: "Orange",
-    company: "Amazon"
-}).then(getDevelopers)
+     company: "Amazon"
+ }).then(getDevelopers)
 .catch(err => console.error(err))
-.finally("We're done"); */
+.finally("We're done"); 
 //Async Await
 async function initProgram() {
     await createDeveloper({
